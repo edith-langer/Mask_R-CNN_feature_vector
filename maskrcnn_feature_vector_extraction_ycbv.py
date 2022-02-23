6e8af6c2cb2db14b77787179dc876179658e232f
Edith <modify.langer@gmail.com>
2022-02-23 11:33:46 +0100

    change model.py to return first FC

--- maskrcnn_feature_vector_extraction_ycbv.py
+++ maskrcnn_feature_vector_extraction_ycbv.py
@@ -4,6 +4,7 @@
 
 # Change path to Mask RCNN directory:
 
+
 import os
 
 # Root directory of the project
@@ -28,9 +29,9 @@ from mrcnn import utils
 import mrcnn.model as modellib
 from mrcnn import visualize
 
-# Import COCO config
-sys.path.append(os.path.join(ROOT_DIR, "samples/coco/"))  # To find local version
-import coco
+# Import YCBV config
+sys.path.append(os.path.join(ROOT_DIR, "samples/ycbv/"))  # To find local version
+import ycbv
 
 # get_ipython().run_line_magic('matplotlib', 'inline')
 
@@ -38,10 +39,8 @@ import coco
 MODEL_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Local path to trained weights file
-COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
-# Download COCO trained weights from Releases if needed
-if not os.path.exists(COCO_MODEL_PATH):
-    utils.download_trained_weights(COCO_MODEL_PATH)
+YCBV_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_ycbv_0005.h5")
+# TODO error if path does not exist
 
 # Directory of images to run detection on
 # IMAGE_DIR = os.path.join(ROOT_DIR, "images")
@@ -49,7 +48,7 @@ rooms = ["/home/workspace/data/GH30_office/", "/home/workspace/data/GH30_living/
          "/home/workspace/data/KennyLab", "/home/workspace/data/Arena/"]
 
 
-class InferenceConfig(coco.CocoConfig):
+class InferenceConfig(ycbv.YCBVConfig):
     # Set batch size to 1 since we'll be running inference on
     # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
     GPU_COUNT = 1
@@ -62,32 +61,21 @@ config.display()
 # Create model object in inference mode.
 model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
 
-# Load weights trained on MS-COCO
-model.load_weights(COCO_MODEL_PATH, by_name=True)
+# Load weights trained on YCBV
+model.load_weights(YCBV_MODEL_PATH, by_name=True)
 
-# COCO Class names
+# YCBV Class names
 # Index of the class in the list is its ID. For example, to get ID of
 # the teddy bear class, use: class_names.index('teddy bear')
-class_names = ['BG', 'person', 'bicycle', 'car', 'motorcycle', 'airplane',
-               'bus', 'train', 'truck', 'boat', 'traffic light',
-               'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird',
-               'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear',
-               'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie',
-               'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
-               'kite', 'baseball bat', 'baseball glove', 'skateboard',
-               'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
-               'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
-               'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
-               'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed',
-               'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote',
-               'keyboard', 'cell phone', 'microwave', 'oven', 'toaster',
-               'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors',
-               'teddy bear', 'hair drier', 'toothbrush']
+class_names = ['BG', 'master_chef_can', 'cracker_box', 'sugar_box', 'tomato_soup_can', 'mustard_bottle',
+               'tuna_fish_can',
+               'pudding_box', 'gelatin_box', 'potted_meat_can', 'banana', 'pitcher', 'bleach_cleanser', 'bowl', 'mug',
+               'power_drill', 'wood_block', 'scissors', 'large_marker', 'large_clamp', 'extra_large_clamp',
+               'foam_brick']
 
 # ## Run Object Detection
 
 for dataset_path in rooms:
-
     scene_paths = [f.path for f in os.scandir(dataset_path) if f.is_dir()]
     scene_paths = [s + '/rgb/' for s in scene_paths]
 
@@ -194,3 +182,4 @@ for dataset_path in rooms:
 
                 except Exception as e:
                     print(e)
+
